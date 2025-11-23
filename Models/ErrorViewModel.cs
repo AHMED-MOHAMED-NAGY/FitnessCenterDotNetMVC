24d6e5bc1cb72d0ca759d24c181015faaccfd389
ahmed <ahmedmohamed6x5@gmail.com>
2025-11-23 16:41:34 +0300

    Cearate Mvc project and add README file

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace fitnessCenter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
